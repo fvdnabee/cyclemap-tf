8fd349b1a4555038eef0c67c6253161ab246beba
Floris Van den Abeele <floris@vdna.be>
2021-07-06 14:07:32 +0200

    Force image pull in ECS agent

--- templates/user-data.sh
+++ templates/user-data.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 # ECS config
+# See https://docs.aws.amazon.com/AmazonECS/latest/developerguide/ecs-agent-config.html
 {
   echo "ECS_CLUSTER=${cluster_name}"
+  echo "ECS_IMAGE_PULL_BEHAVIOR=always"
 } >> /etc/ecs/ecs.config
 
 start ecs
